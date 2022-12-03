e6db277731e0c2c6812ffae54af7b7aa7369736d
pvlncharan <pvlnc98@gmail.com>
2022-12-03 14:08:59 +0530

    We start Javascript assignment that 1 day task is completed

---
+++ Day-2-mathematical/sum1.js
@@ -0,0 +1,7 @@
+// Description
+//     You need to print "TTF SERVICES" on a line, and print "Working with metavarse" on a new line.
+
+//output  = TTF SERVICES
+//         Working with metavarse;
+
+console.log(" TTF SERVICES \n Working with metavarse");
---
+++ Day-2-mathematical/sum5.js
@@ -0,0 +1,14 @@
+// Description
+        // Given a number N, find its cube.
+
+
+function fineCube(x){
+    //write your code here
+    console.log("x =",x)
+    var cube = x**3;
+    console.log("cube = ",cube);
+   
+}
+
+var x = 6;
+fineCube(x)
